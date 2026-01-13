62984e69708c8462513f8893600d6a7929c04490
aritriana <triana.ari89@gmail.com>
2026-01-13 13:04:10 +0700

    Menambahkan logika or || pada code

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 //   console.log('Yah biasa aja');
 // }
 
-const password = prompt("Buatlah Password?");
+// const password = prompt("Buatlah Password?");
 
 // Password harus sepanjang 6 karakter
 // if (password.length >= 6) {
@@ -21,8 +21,16 @@ const password = prompt("Buatlah Password?");
 //   console.log("Password minimal 6 karakter");
 // }
 
-if (password.length >= 6 && password.indexOf(" ") === -1) {
-  console.log("Password Valid");
+// if (password.length >= 6 && password.indexOf(" ") === -1) {
+//   console.log("Password Valid");
+// } else {
+//   console.log("Password tidak memenuhi syarat");
+// }
+
+const role = prompt("Masukkan role akun?").toLowerCase();
+
+if (role == "admin" || role == "spv") {
+  console.log("Akses diterima");
 } else {
-  console.log("Password tidak memenuhi syarat");
+  console.log("Akses ditolak");
 }
