c29ba55f218400e019b6656a4a42bade79474f3e
aritriana <triana.ari89@gmail.com>
2026-01-13 08:17:28 +0700

    Menjalankan perintah elseif dengan fungsi prompt

---
+++ app.js
@@ -0,0 +1,9 @@
+const iniHari = prompt('Isikan hari?').toLowerCase();
+
+if (iniHari === 'senin'){
+  console.log('Selamat bekerja kawan!!');
+} else if (iniHari ==='sabtu'){
+  console.log('Semoga jangan cepat berlalu');
+} else {
+  console.log('Yah biasa aja');
+}
