c057423cbcd8468fda8a79bb4c556bb53f6d8298
aritriana <triana.ari89@gmail.com>
2026-01-13 12:44:57 +0700

    Menambahkan perintah untuk validasi panjang password

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-const iniHari = prompt('Isikan hari?').toLowerCase();
+// const iniHari = prompt('Isikan hari?').toLowerCase();
 
-if (iniHari === 'senin'){
-  console.log('Selamat bekerja kawan!!');
-} else if (iniHari ==='sabtu'){
-  console.log('Semoga jangan cepat berlalu');
+// if (iniHari === 'senin'){
+//   console.log('Selamat bekerja kawan!!');
+// } else if (iniHari ==='sabtu'){
+//   console.log('Semoga jangan cepat berlalu');
+// } else {
+//   console.log('Yah biasa aja');
+// }
+
+const password = prompt("Buatlah Password?");
+
+// Password harus sepanjang 6 karakter
+if (password.length >= 6) {
+  if (password.indexOf(" ") === -1) {
+    console.log("Password valid");
+  } else {
+    console.log("Password tidak boleh ada spasi");
+  }
 } else {
-  console.log('Yah biasa aja');
+  console.log("Password minimal 6 karakter");
 }
